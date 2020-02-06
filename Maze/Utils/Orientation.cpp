227f91f6e43a36a4deaee75f59defe8200f96544
Hugo Majerczyk <Superseth794@gmail.com>
2020-02-06 23:45:40 +0100

    Added Util class to represent possible orientations

---
+++ Maze/Utils/Orientation.cpp
@@ -0,0 +1,58 @@
+//
+//  Orientation.cpp
+//  Maze
+//
+//  Created by Jaraxus on 06/02/2020.
+//
+
+# include "Orientation.hpp"
+
+namespace mz {
+
+const Orientation Orientation::UP("UP", sf::Vector2f(0, -1));
+const Orientation Orientation::RIGHT("RIGHT", sf::Vector2f(1, 0));
+const Orientation Orientation::DOWN("DOWN", sf::Vector2f(0, 1));
+const Orientation Orientation::LEFT("LEFT", sf::Vector2f(-1, 0));
+const Orientation Orientation::UNDEFINED("UNDEFINED", sf::Vector2f(0.f, 0.f));
+
+Orientation::Orientation(std::string const& name, sf::Vector2f vector) :
+m_name(name),
+m_direction(vector)
+{
+}
+
+std::string const& Orientation::getName() const{
+    return m_name;
+}
+
+sf::Vector2f const& Orientation::toVector() const{
+    return m_direction;
+}
+
+Orientation const& Orientation::getOrientation(std::string const& name) {
+    if (name == UP.getName())
+        return UP;
+    else if (name == RIGHT.getName())
+        return RIGHT;
+    else if (name == DOWN.getName())
+        return DOWN;
+    else if (name == LEFT.getName())
+        return LEFT;
+    else
+        return UNDEFINED;
+}
+
+Orientation const& Orientation::getOrientation(sf::Vector2f const& direction) {
+    if (direction == UP.toVector())
+        return UP;
+    else if (direction == RIGHT.toVector())
+        return RIGHT;
+    else if (direction == DOWN.toVector())
+        return DOWN;
+    else if (direction == LEFT.toVector())
+        return LEFT;
+    else
+        return UNDEFINED;
+}
+
+}
---
+++ Maze/Utils/Orientation.hpp
@@ -0,0 +1,42 @@
+//
+//  Orientation.hpp
+//  Maze
+//
+//  Created by Jaraxus on 06/02/2020.
+//
+
+#ifndef Orientation_h
+# define Orientation_h
+
+# include <SFML/Graphics.hpp>
+
+namespace mz {
+
+class Orientation {
+public:
+    
+    static const Orientation UP;
+    static const Orientation RIGHT;
+    static const Orientation DOWN;
+    static const Orientation LEFT;
+    static const Orientation UNDEFINED;
+    
+    std::string const& getName() const;
+    sf::Vector2f const& toVector() const;
+    
+    static Orientation const& getOrientation(std::string const& name);
+    static Orientation const& getOrientation(sf::Vector2f const& direction);
+    
+private:
+    Orientation(std::string const& name, sf::Vector2f vector);
+    
+private:
+    std::string m_name;
+    sf::Vector2f m_direction;
+    
+};
+
+
+}
+
+#endif /* Orientation_h */
