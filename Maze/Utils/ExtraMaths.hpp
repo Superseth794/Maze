8bbe2eec0aec09f52d4a2d6fbf1dd2d8a36e44ef
Hugo Majerczyk <Superseth794@gmail.com>
2020-06-29 18:19:52 +0200

    Add scalar product

--- Maze/Utils/ExtraMaths.hpp
+++ Maze/Utils/ExtraMaths.hpp
@@ -10,30 +10,30 @@
 
 # include <cmath>
 
-# include <SFML/Graphics.hpp>
+# include <SFML/System.hpp>
 
 # ifndef M_PI
-#  define M_PI 3.14159265359
+#  define M_PI 3.14159265359 //> pi
 # endif
 
 # ifndef M_PI_2
-#  define M_PI_2 1.57079632679
+#  define M_PI_2 1.57079632679 //> sqrt(pi)
 # endif
 
 # ifndef M_PI2
-#  define M_PI2 9.86960440109
+#  define M_PI2 9.86960440109 //> pi^2
 # endif
 
 # ifndef M_PI3
-#  define M_PI3 31.0062766803
+#  define M_PI3 31.0062766803 //> pi^3
 # endif
 
-# ifndef M_EXP
-#  define M_EXP 2.71828182846
+# ifndef M_E
+#  define M_E 2.71828182846 //> e
 # endif
 
 # ifndef M_SQRT2
-#  define M_SQRT2 1.4142135623730950488016887
+#  define M_SQRT2 1.4142135623730950488016887 //> sqrt(2)
 # endif
 
 namespace mz {
@@ -71,7 +71,9 @@ template<typename T> constexpr T getVectorLength2(sf::Vector2<T> const& vect) {
     return (vect.x * vect.x + vect.y * vect.y);
 }
 
-// TODO add scalar product
+template<typename T> constexpr T getScalarProduct(sf::Vector2<T> const& vect1, sf::Vector2<T> const& vect2) {
+    return (vect1.x * vect2.x) + (vect1.y * vect2.y);
+}
 
 }
 
