e96af1b894bb14761c2dcdd9fb445a0e97a9c04d
Hugo Majerczyk <Superseth794@gmail.com>
2020-07-01 22:11:04 +0200

    Add visual tests session

---
+++ Maze/Tests/VisualTests/VisualTestsSession.cpp
@@ -0,0 +1,8 @@
+//
+//  VisualTestsSession.cpp
+//  Maze
+//
+//  Created by Jaraxus on 01/07/2020.
+//
+
+#include "VisualTestsSession.hpp"
---
+++ Maze/Tests/VisualTests/VisualTestsSession.hpp
@@ -0,0 +1,13 @@
+//
+//  VisualTestsSession.hpp
+//  Maze
+//
+//  Created by Jaraxus on 01/07/2020.
+//
+
+#ifndef VisualTestsSession_hpp
+#define VisualTestsSession_hpp
+
+#include <stdio.h>
+
+#endif /* VisualTestsSession_hpp */
