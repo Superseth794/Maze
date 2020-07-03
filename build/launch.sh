71a61ed21a6f39b37b9dcc711890c97a1d3ef24a
Hugo Majerczyk <Superseth794@gmail.com>
2020-07-03 16:56:33 +0200

    Updtae launch script and .gitignore

--- build/launch.sh
+++ build/launch.sh
@@ -1,7 +1,9 @@
-/Users/jaraxus/Downloads/premake5 --file=premake5.lua gmake2
+./premake5 --file=premake5.lua gmake2
+
 echo "\n----- Premake loaded -----\n"
 make
 echo "\n----- Make achieved -----\n"
+
 echo "----- Starting program -----\n"
 export LD_LIBRARY_PATH=../../../SFML/lib
 cd bin/Debug/
