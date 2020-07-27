bd6a7265f0d62052d5a17591193cb4b0d2284757
Hugo Majerczyk <Superseth794@gmail.com>
2020-07-27 16:36:50 +0200

    Refactoring - fix missing included files and add compiler warnings

--- build/premake5.lua
+++ build/premake5.lua
@@ -15,9 +15,56 @@ project "Maze"
 	language "C++"
 	cppdialect("C++17")
 
+	if os.execute("gcc -v") == 0 then
+		toolset "gcc"
+	end
+
+	buildoptions {
+		"-pedantic",
+        "-pedantic-errors",
+        "-Wall",
+        "-Wextra",
+
+        "-Warray-bounds",
+        "-Wcast-align",
+        "-Wcast-qual",
+        "-Wconditionally-supported",
+        "-Wconversion",
+        "-Wdisabled-optimization",
+        "-Wdouble-promotion",
+        "-Wfloat-conversion",
+        "-Wformat=2",
+        "-Wformat-security",
+        "-Wlogical-op",
+        "-Wmissing-declarations",
+        "-Wmissing-include-dirs",
+        "-Wnoexcept",
+        "-Wnon-virtual-dtor",
+        "-Wold-style-cast",
+        "-Wopenmp-simd",
+        "-Woverloaded-virtual",
+        "-Wpacked",
+        "-Wredundant-decls",
+        "-Wstrict-aliasing",
+        "-Wstrict-null-sentinel",
+        "-Wsuggest-final-methods",
+        "-Wsuggest-final-types",
+        "-Wtrampolines",
+        "-Wundef",
+        "-Wuninitialized",
+        "-Wunused-macros",
+        "-Wuseless-cast",
+        "-Wvector-operation-performance",
+        "-Wvla",
+        "-Wzero-as-null-pointer-constant",
+        "-Wno-comment",
+        "-Wno-format-nonliteral"
+	}
+
 	files {
 		"../Maze/**.cpp",
-		"../Maze/**.hpp"
+		"../Maze/**.hpp",
+		"../Maze/**.inl"
 	}
 
 	includedirs {
